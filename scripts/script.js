94945a72a0b687029673da1db60d3a35c663ecfd
Roman Gontar <ashenoooone@gmail.com>
2021-12-01 14:36:28 +0400

    fix: пофиксил замечания ревьювера в файле script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,33 +4,28 @@ const formCloseButton = document.querySelector(".form__close-button");
 const formSaveButton = document.querySelector(".form__save-button");
 const likeButtons = document.querySelectorAll(".elements__like-button");
 const formOverlay = document.querySelector(".form__overlay");
+const formName = document.querySelector(".form__name");
+const formActivity = document.querySelector(".form__activity");
+const profileName = document.querySelector(".profile__name");
+const profileActivity = document.querySelector(".profile__activity");
 
 function formOpen() {
   form.classList.add("form_active");
-  document.body.classList.add("hidden");
+  formName = profileName;
+  formActivity = profileActivity;
 }
 
 function formClose() {
   form.classList.remove("form_active");
-  document.body.classList.remove("hidden");
 }
 function formSubmit(evt) {
   evt.preventDefault();
-  let formName = document.querySelector(".form__name").value;
-  let formActivity = document.querySelector(".form__activity").value;
-  let profileName = document.querySelector(".profile__name");
-  let profileActivity = document.querySelector(".profile__activity");
-  profileName.textContent = formName;
-  profileActivity.textContent = formActivity;
-  form.classList.remove("form_active");
-  document.body.classList.remove("hidden");
+
+  profileName.textContent = formName.value;
+  profileActivity.textContent = formActivity.value;
+  formClose();
 }
 formOpenButton.addEventListener("click", formOpen);
 formCloseButton.addEventListener("click", formClose);
 form.addEventListener("submit", formSubmit);
 formOverlay.addEventListener("click", formClose);
-likeButtons.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("elements__like-button_active");
-  });
-});
