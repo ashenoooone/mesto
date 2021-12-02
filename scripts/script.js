972ff93bbd39c4cb1eefc1603763bac279880738
Roman Gontar <ashenoooone@gmail.com>
2021-12-02 16:59:50 +0400

    fix: удалил лишнюю переменную в Js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ const popup = document.querySelector(".popup");
 const popupForm = document.querySelector(".popup__form");
 const popupOpenButton = document.querySelector(".profile__edit-button");
 const popupCloseButton = document.querySelector(".popup__close-button");
-const popupSaveButton = document.querySelector(".popup__save-button");
 const popupName = document.querySelector(".popup__input_type_name");
 const popupActivity = document.querySelector(".popup__input_type_activity");
 const profileName = document.querySelector(".profile__name");
