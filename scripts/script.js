b8cb3ff15dfefe047fc363b87a820c2199e3807d
Roman Gontar <ashenoooone@gmail.com>
2021-12-11 16:25:48 +0400

    feat: сделал второй popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
-const popup = document.querySelector(".popup");
 const popupForm = document.querySelector(".popup__form");
-const popupOpenButton = document.querySelector(".profile__edit-button");
-const popupCloseButton = document.querySelector(".popup__close-button");
+const editButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelectorAll(".popup__close-button");
 const popupName = document.querySelector(".popup__input_type_name");
 const popupActivity = document.querySelector(".popup__input_type_activity");
 const profileName = document.querySelector(".profile__name");
 const profileActivity = document.querySelector(".profile__activity");
 const likeButton = document.querySelectorAll(".card__like-button");
 const addButton = document.querySelector(".profile__add-button");
+const popupAddCard = document.querySelector(".popup_type_add-card");
+const popupEditProfile = document.querySelector(".popup_type_edit-profile");
 
-function popupOpen() {
-  popup.classList.add("popup_active");
-  popupName.value = profileName.textContent;
-  popupActivity.value = profileActivity.textContent;
-}
-
-function popupClose() {
-  popup.classList.remove("popup_active");
-}
 function popupSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileActivity.textContent = popupActivity.value;
   popupClose();
 }
-popupCloseButton.addEventListener("click", popupClose);
-popupOpenButton.addEventListener("click", popupOpen);
+popupCloseButton.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.closest(".popup").classList.remove("popup_active");
+  });
+});
+editButton.addEventListener("click", () => {
+  popupEditProfile.classList.add("popup_active");
+});
 popupForm.addEventListener("submit", popupSubmit);
-addButton.addEventListener("click", popupOpen);
+addButton.addEventListener("click", () => {
+  popupAddCard.classList.add("popup_active");
+});
 likeButton.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("card__like-button_active");
