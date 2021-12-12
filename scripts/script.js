e9f09339a3eea6c29c888873dc1115e6fda976d9
Roman Gontar <ashenoooone@gmail.com>
2021-12-12 18:27:00 +0400

    feat: открытие popup'a картинки при нажатии по карточке

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,8 @@ const elementsList = document.querySelector(".elements");
 const cardTemplate = document.querySelector(".template__card");
 const popupAddName = document.querySelector(".popup__input_type_name");
 const popupAddLink = document.querySelector(".popup__input_type_link");
+const templateCardZoom = document.querySelector(".template__card-zoom");
+const page = document.querySelector(".page");
 const initialCards = [
   {
     name: "Архыз",
@@ -39,57 +41,112 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+
 function renderInitialElemens() {
   for (let i = 0; i < initialCards.length; i++) {
     const newItem = cardTemplate.content.cloneNode(true).querySelector(".card");
-    newItem.querySelector(".card__title").textContent = initialCards[i].name;
-    newItem.querySelector(".card__image").alt = initialCards[i].name;
-    newItem.querySelector(".card__image").src = initialCards[i].link;
-    newItem
-      .querySelector(".card__like-button")
-      .addEventListener("click", (evt) => {
-        evt.target.classList.toggle("card__like-button_active");
-      });
-    newItem
-      .querySelector(".card__delete-button")
-      .addEventListener("click", (evt) => {
-        evt.target.parentElement.remove();
-      });
+    const newItemTitle = newItem.querySelector(".card__title");
+    const newItemImage = newItem.querySelector(".card__image");
+    const newItemLikeButton = newItem.querySelector(".card__like-button");
+    const newItemDeleteButton = newItem.querySelector(".card__delete-button");
+    const newItemZoomedCard = templateCardZoom.content
+      .cloneNode(true)
+      .querySelector(".popup");
+    const newItemZoomedCardName =
+      newItemZoomedCard.querySelector(".zoomedCard__title");
+    const newItemZoomedCardImage =
+      newItemZoomedCard.querySelector(".zoomedCard__image");
+    const newItemZoomedCardCloseButton = newItemZoomedCard.querySelector(
+      ".popup__close-button"
+    );
+    newItemTitle.textContent = initialCards[i].name;
+    newItemImage.alt = initialCards[i].name;
+    newItemImage.src = initialCards[i].link;
+    newItemZoomedCard.id = newItemTitle.textContent;
+    newItemZoomedCardName.textContent = newItemTitle.textContent;
+    newItemZoomedCardImage.src = newItemImage.src;
+    newItemZoomedCardImage.alt = newItemTitle.textContent;
+    newItemZoomedCardCloseButton.addEventListener("click", (evt) => {
+      evt.currentTarget.closest(".popup").classList.remove("popup_active");
+    });
+    newItem.addEventListener("click", (evt) => {
+      if (
+        newItemZoomedCard.id == newItemTitle.textContent &&
+        evt.target.classList.contains("card")
+      ) {
+        newItemZoomedCard.classList.add("popup_active");
+      }
+    });
+    newItemLikeButton.addEventListener("click", (evt) => {
+      evt.target.classList.toggle("card__like-button_active");
+    });
+    newItemDeleteButton.addEventListener("click", (evt) => {
+      evt.target.parentElement.remove();
+    });
+    page.insertAdjacentElement("afterend", newItemZoomedCard);
     elementsList.insertAdjacentElement("beforeend", newItem);
   }
 }
+
 renderInitialElemens();
 
 function popupAddCardSubmit(evt) {
   evt.preventDefault();
   const newCard = cardTemplate.content.cloneNode(true).querySelector(".card");
-  const newCardName = newCard.querySelector(".card__title");
+  const newCardTitle = newCard.querySelector(".card__title");
   const newCardImage = newCard.querySelector(".card__image");
-  newCardName.textContent = popupAddName.value;
-  newCardImage.src = popupAddName.value;
+  const newCardLikeButton = newCard.querySelector(".card__like-button");
+  const newCardDeleteButton = newCard.querySelector(".card__delete-button");
+  const newItemZoomedCard = templateCardZoom.content
+    .cloneNode(true)
+    .querySelector(".popup");
+  const newItemZoomedCardName =
+    newItemZoomedCard.querySelector(".zoomedCard__title");
+  const newItemZoomedCardImage =
+    newItemZoomedCard.querySelector(".zoomedCard__image");
+  const newItemZoomedCardCloseButton = newItemZoomedCard.querySelector(
+    ".popup__close-button"
+  );
+  newCardTitle.textContent = popupAddName.value;
+  newCardImage.alt = popupAddName.value;
   newCardImage.src = popupAddLink.value;
-  newCard
-    .querySelector(".card__like-button")
-    .addEventListener("click", (evt) => {
-      evt.target.classList.toggle("card__like-button_active");
-    });
-  newCard
-    .querySelector(".card__delete-button")
-    .addEventListener("click", (evt) => {
-      evt.target.parentElement.remove();
-    });
+  newItemZoomedCard.id = newCardTitle.textContent;
+  newItemZoomedCardName.textContent = newCardTitle.textContent;
+  newItemZoomedCardImage.src = newCardImage.src;
+  newItemZoomedCardImage.alt = newCardTitle.textContent;
+  newItemZoomedCardCloseButton.addEventListener("click", (evt) => {
+    evt.currentTarget.closest(".popup").classList.remove("popup_active");
+  });
+  newCard.addEventListener("click", (evt) => {
+    if (
+      newItemZoomedCard.id == newCardTitle.textContent &&
+      evt.target.classList.contains("card")
+    ) {
+      newItemZoomedCard.classList.add("popup_active");
+    }
+  });
+  newCardLikeButton.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("card__like-button_active");
+  });
+  newCardDeleteButton.addEventListener("click", (evt) => {
+    evt.target.parentElement.remove();
+  });
+  page.insertAdjacentElement("afterend", newItemZoomedCard);
   elementsList.insertAdjacentElement("beforeend", newCard);
   evt.target.closest(".popup").classList.remove("popup_active");
 }
 
 function popupEditProfileSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = this.querySelector(".popup__input_type_name").value;
-  profileActivity.textContent = this.querySelector(
+  profileName.textContent = evt.target.querySelector(
+    ".popup__input_type_name"
+  ).value;
+  profileActivity.textContent = evt.target.querySelector(
     ".popup__input_type_activity"
   ).value;
   evt.target.closest(".popup").classList.remove("popup_active");
 }
+
 popupCloseButton.forEach((item) => {
   item.addEventListener("click", () => {
     item.closest(".popup").classList.remove("popup_active");
